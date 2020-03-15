2cb6d9eced382264bc5c0e7401f1a4c393ed197b
Anıl Şenay <anilsenay3@gmail.com>
2020-03-15 12:50:57 +0300

    Updates

---
+++ precache-manifest.5d9b96a9ebd7a8356e58483890716d92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68fb6013df8d0e0e4559aa8ed0081f59",
+    "url": "/anilsenay.wtf/index.html"
+  },
+  {
+    "revision": "c4d4d61e1dfaa222a891",
+    "url": "/anilsenay.wtf/static/css/main.47ca54d0.chunk.css"
+  },
+  {
+    "revision": "c6ec7520bf4b77e36b4d",
+    "url": "/anilsenay.wtf/static/js/2.c1064278.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/anilsenay.wtf/static/js/2.c1064278.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4d4d61e1dfaa222a891",
+    "url": "/anilsenay.wtf/static/js/main.aa0551d7.chunk.js"
+  },
+  {
+    "revision": "14bd7549183901dab996",
+    "url": "/anilsenay.wtf/static/js/runtime-main.462ff11e.js"
+  }
+]);
